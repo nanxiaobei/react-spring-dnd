ee2bdbcdebc9a088155472ed3430f8a45e3d5e01
nanxiaobei <nanxiaobei@gmail.com>
2023-02-26 06:00:13 +0800

    fix configs

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,21 @@
-import dts from 'rollup-plugin-dts';
+import type { InputPluginOption, RollupOptions } from 'rollup';
+import { generateDtsBundle as dts } from 'rollup-plugin-dts-bundle-generator';
 import typescript from '@rollup/plugin-typescript';
 import { readFileSync } from 'fs';
 
 const pkg = JSON.parse(readFileSync('./package.json') as unknown as string);
 
 const input = 'src/index.tsx';
-const cjsOutput = { file: pkg.main, format: 'cjs', exports: 'auto' };
-const esmOutput = { file: pkg.module, format: 'es' };
-const dtsOutput = { file: pkg.types, format: 'es' };
+const cjsOutput = { file: pkg.main, format: 'cjs', exports: 'auto' } as const;
+const esmOutput = { file: pkg.module, format: 'es' } as const;
 
-export default [
-  { input, output: cjsOutput, plugins: [typescript()], external: () => true },
-  { input, output: esmOutput, plugins: [typescript()], external: () => true },
-  { input, output: dtsOutput, plugins: [dts()] },
+const tsPlugin = typescript();
+const dtsPlugin = dts({ outFile: pkg.types }) as InputPluginOption;
+const external = () => true;
+
+const config: RollupOptions[] = [
+  { input, output: cjsOutput, plugins: [tsPlugin], external },
+  { input, output: esmOutput, plugins: [tsPlugin, dtsPlugin], external },
 ];
+
+export default config;
